316a306dcba8a5b3ecd41c44ec2b7c7d8bd37061
leeahyeon <leeahyeon01@gmail.com>
2023-08-12 19:51:54 +0900

    frist commit

---
+++ src/main/java/com/example/pharmacyfindproject/PharmacyfindProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyfindproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyfindProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyfindProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pharmacyfindproject/PharmacyfindProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyfindproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyfindProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
